4955553126c07fda4c75a80c7e2ab77cd130f0ea
Andrew <andromedakroos@gmail.com>
2022-12-04 00:48:25 +0100

    initial commit

---
+++ src/main/java/Factory/SimpleDriver.java
@@ -0,0 +1,14 @@
+package Factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleDriver {
+
+    public WebDriver getDriver(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\andro\\IdeaProjects\\TestingTNGUnitCourses\\src\\test\\resources\\chromedriver.exe");
+        return new ChromeDriver();
+    }
+
+
+}
---
+++ src/test/java/tests/SimpleWDTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import Factory.AdvancedDriver;
+import Factory.BrowserFactory;
+import Factory.SimpleDriver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class SimpleWDTest {
+    @Test
+    public void simpleWDTest(){
+        SimpleDriver simpleDriver = new SimpleDriver();
+        WebDriver driver = simpleDriver.getDriver();
+        driver.quit();
+    }
+
+    @Test
+    public void advancedWDTest(){
+        AdvancedDriver advancedDriver = new AdvancedDriver();
+        WebDriver driver = advancedDriver.getDriver();
+        driver.quit();
+    }
+
+    @Test
+    public void browserFactoryTest(){
+        BrowserFactory browserFactory = new BrowserFactory();
+        WebDriver driver = browserFactory.getDriver();
+        driver.quit();
+    }
+}
