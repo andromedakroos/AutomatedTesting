3d2acb4fcd6810a5b2f85999d002903de04f48e8
Andrew <andromedakroos@gmail.com>
2022-12-04 02:34:20 +0100

    Add and Delete element tests

--- src/test/java/tests/AddAndDeleteElementTest.java
+++ src/test/java/tests/AddAndDeleteElementTest.java
@@ -1,4 +1,19 @@
 package tests;
 
+import factory.BrowserFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
 public class AddAndDeleteElementTest {
+    private BrowserFactory browserFactory = new BrowserFactory();
+    private WebDriver driver = browserFactory.getDriver();
+
+    @Test
+    public void AddAndDeleteElementsTest(){
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        driver.findElement(By.xpath("//button[text()='Add Element']")).click();
+        driver.findElement(By.xpath("//button[text()='Add Element']")).click();
+    }
+
 }
