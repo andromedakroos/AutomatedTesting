536835495eb5acde4bbe364ad4a0e922a52125ab
Andrew <andromedakroos@gmail.com>
2022-12-07 02:31:58 +0100

    warm floor calc test

--- src/test/java/tests/WarmFloorCalcTest.java
+++ src/test/java/tests/WarmFloorCalcTest.java
@@ -3,8 +3,12 @@ package tests;
 import factory.BrowserFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class WarmFloorCalcTest {
     private WebDriver driver;
@@ -14,16 +18,38 @@ public class WarmFloorCalcTest {
         BrowserFactory browserFactory = new BrowserFactory();
         driver = browserFactory.getDriver();
     }
-
+    @Test
     public void warmFloorCalculatorTest(){
         driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
+
         driver.findElement(By.id("el_f_width")).sendKeys("4");
         driver.findElement(By.id("el_f_lenght")).sendKeys("5");
+
+        WebElement roomType = driver.findElement(By.id("room_type"));
+        Select selectRoomType = new Select(roomType);
+        selectRoomType.selectByValue("2");
+
+
+        WebElement heatingType = driver.findElement(By.id("heating_type"));
+        Select selectHeatingType = new Select(heatingType);
+        selectHeatingType.selectByValue("3");
+
+        driver.findElement(By.id("el_f_losses")).sendKeys("2141");
+        driver.findElement(By.cssSelector("[name='button']")).click();
+
+//        Assert.assertEquals(driver.findElement(By.id("floor_cable_power")).getText(),
+//                "1191");
+//
+//        Assert.assertEquals(driver.findElement(By.id("spec_floor_cable_power")).getText(),
+//                "60");
+
+        System.out.println(driver.findElement(By.id("floor_cable_power")).getText());
+
     }
 
     @AfterMethod
     public void tearDown() throws InterruptedException {
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         driver.quit();
     }
 }
