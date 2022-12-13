d289ee1001316cd2684e28789a86ffb483905a45
Andrew Moroz <andromedakroos@gmail.com>
2022-12-13 10:58:05 +0100

    Laminate calculator test

--- src/test/java/tests/LaminateCalculatorTest.java
+++ src/test/java/tests/LaminateCalculatorTest.java
@@ -62,6 +62,6 @@ public class LaminateCalculatorTest {
     @AfterMethod
     public void tearDown() throws InterruptedException {
         Thread.sleep(5000);
-//        driver.quit();
+        driver.quit();
     }
 }
