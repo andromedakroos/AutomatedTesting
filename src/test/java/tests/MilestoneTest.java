20357e4f06152599a5fba1c22d87aab1dddcc867
Andrew Moroz <andromedakroos@gmail.com>
2023-01-24 17:40:31 +0100

    lombok final

--- src/test/java/tests/MilestoneTest.java
+++ src/test/java/tests/MilestoneTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import models.Milestone;
+import models.MilestoneBuilder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,6 +20,23 @@ public class MilestoneTest {
 
         System.out.println(milestone1);
         System.out.println(milestone2);
+        Assert.assertTrue(milestone1.equals(milestone2));
+    }
+    @Test
+    public void lombokTest2(){
+        MilestoneBuilder milestone1 = MilestoneBuilder.builder()
+                .id(3)
+                .name("ml_02")
+                .completed(false)
+                .build();
+        System.out.println(milestone1.toString());
+
+        MilestoneBuilder milestone2 = MilestoneBuilder.builder()
+                .id(4)
+                .name("ml_02")
+                .completed(false)
+                .build();
+        System.out.println(milestone2.toString());
         Assert.assertTrue(milestone1.equals(milestone2));
     }
 }
