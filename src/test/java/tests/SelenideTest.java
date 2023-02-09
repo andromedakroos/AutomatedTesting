ea9d5819a158fcdde34e77cc51016479cbc33d1f
Andrew Moroz <andromedakroos@gmail.com>
2023-02-09 17:03:12 +0100

    selenide

--- src/test/java/tests/SelenideTest.java
+++ src/test/java/tests/SelenideTest.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.CollectionCondition.empty;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byTitle;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
@@ -32,15 +34,46 @@ public class SelenideTest extends BaseTest {
                 .shouldBe(enabled)
                 .click();
 
-/*
-        $(withText("All Projects"))
-                .shouldBe(visible)
-                .shouldHave(text("All Projects"));
-*/
+//        $(withText("All Projects"))
+//                .shouldBe(visible)
+//                .shouldHave(text("All Projects"));
 
         $(By.xpath("//div[contains(text(), 'All Projects')]"))
                 .shouldBe(visible)
                 .shouldHave(text("All Projects"));
+
+        $$(withText("All Projects"))
+                .shouldBe(empty)
+                .shouldBe(size(10))
+                .texts();
+
+
+
+
+
+
+
+
+
+
+//        $(By.id("name")).setValue(ReadProperties.username());
+//        $("#password").setValue(ReadProperties.password());
+//
+//        SelenideElement loginButton = $("#button_primary");
+//        loginButton
+//                .should(exist)
+//                .shouldBe(enabled)
+//                .click();
+//
+///*
+//        $(withText("All Projects"))
+//                .shouldBe(visible)
+//                .shouldHave(text("All Projects"));
+//*/
+//
+//        $(By.xpath("//div[contains(text(), 'All Projects')]"))
+//                .shouldBe(visible)
+//                .shouldHave(text("All Projects"));
 
 /*
         $$(withText("All Projects"))
